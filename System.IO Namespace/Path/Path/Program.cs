fe846cf7a7174c4bdaa2b8c35ec82d199d4cd4b4
Ellis Clarke <ellisbclarke03@gmail.com>
2021-04-29 14:41:37 +0100

    upload 2

---
+++ System.IO Namespace/Path/Path/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\ellis\Documents\GitHub\CollegeCSharp\System.IO Namespace\Path\Path.sln";
+
+            Console.WriteLine("Extenstion: " + System.IO.Path.GetExtension(path));
+            Console.WriteLine("File Name: " + System.IO.Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + System.IO.Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + System.IO.Path.GetDirectoryName(path));
+        }
+    }
+}

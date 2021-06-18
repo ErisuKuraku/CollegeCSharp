f61adaa9b41d3d14b48993a83073eb3fa86a2c46
Ellis Clarke <ellisbclarke03@gmail.com>
2021-06-18 11:13:58 +0100

    Added UI.cs

---
+++ Unit 4 A2/CollegeApp/CollegeApp/UI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApp
+{
+    class UI
+    {
+        public void mainMenu()
+        {
+            string choice = "start";
+            while (!choice.Equals("quit"))
+            {
+                Console.WriteLine("1 - Enter new participant");
+                Console.WriteLine("or type quit");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter Name: ");
+                        string name = Console.ReadLine();
+                        Participant p = new Participant();
+                        p.setName(name);
+                        p.printParticipant();
+                        break;
+
+                    case "2":
+                        break;
+                }
+            }
+        }
+    }
+}
